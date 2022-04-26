5652769c68d5ea4280ad0b06c73f64dd0d828ce5
Kamie Short <kamie.short@gmail.com>
2022-04-26 15:57:48 -0700

    added search and loader

--- src/views/List/List.jsx
+++ src/views/List/List.jsx
@@ -3,13 +3,19 @@ import { fetchQuotes } from '../../services/fetch';
 
 export default function QuoteList() {
   const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
     const getQuotes = async () => {
       const data = await fetchQuotes();
 
       setQuotes(data);
-      console.log(data);
+      setLoading(false);
     };
 
     getQuotes();
@@ -17,14 +23,22 @@ export default function QuoteList() {
 
   return (
     <>
+      {}
       <h2>Futurama Quotes</h2>
+      {loading ? (
+        <p>Loading..</p>
+      ) : (
+        <>
+          <input placeholder="search" value={search} onChange={handleSearch} />
 
-      {quotes.map((quote) => (
-        <div key={quote.id}>
-          <h2>{quote.character}</h2>
-          <p>{quote.quote}</p>
-        </div>
-      ))}
+          {quotes.map((quote) => (
+            <div key={quote.id}>
+              <h2>{quote.character}</h2>
+              <p>{quote.quote}</p>
+            </div>
+          ))}
+        </>
+      )}
     </>
   );
 }
